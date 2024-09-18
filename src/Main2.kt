13ce186516338e0ef3aa57764d2eed6a9e7b34a8
Polyden <karandeevdenis1@gmail.com>
2024-09-18 16:18:38 +0300

    8

---
+++ src/Main2.kt
@@ -0,0 +1,19 @@
+fun main() {
+    val input = readln();
+    var count = 0;
+    var lastChar = input[0];
+    for(item in input)
+    {
+            if(lastChar == item)
+            {
+                count++;
+            }
+            else
+            {
+                print("$lastChar$count");
+                count = 1;
+            }
+        lastChar = item;
+    }
+    print("$lastChar$count");
+}
